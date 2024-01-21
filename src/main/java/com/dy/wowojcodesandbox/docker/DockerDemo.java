232aee3ea1faf0334326d9d883afc5d97b478031
Ding Yi <dw520521@gmail.com>
2024-01-21 23:33:50 +0800

    docker配置

---
+++ src/main/java/com/dy/wowojcodesandbox/docker/DockerDemo.java
@@ -0,0 +1,14 @@
+package com.dy.wowojcodesandbox.docker;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.PingCmd;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+public class DockerDemo {
+    public static void main(String[] args){
+        //获取默认的DockerClient
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+        PingCmd pingCmd = dockerClient.pingCmd();
+        pingCmd.exec();
+    }
+}
